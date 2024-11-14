64bf7a878d73c56f9d60968bcbd49c024d887fa5
KunoDLK <kunodlk@gmail.com>
2024-11-14 16:03:23 +0000

    Added database

---
+++ Products Service/Controllers/Products.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Runtime.Serialization.DataContracts;
+using LaMaCo.Comments.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products_Service.Data;
+
+namespace Products_Service.Controllers
+{
+      [Route("api/[controller]")]
+      [ApiController]
+      public class Products : ControllerBase
+      {
+            private readonly ProductDbContext _db;
+
+            public Products(ProductDbContext dbContexts)
+            {
+                  _db = dbContexts;
+                  _db.Database.EnsureCreated();
+                  _db.Database.Migrate();
+            }
+
+            [HttpGet]
+            public IEnumerable<Product> GetProducts()
+            {
+                  return _db.Products.ToList();
+            }
+      }
+}
--- Products Service/Program.cs
+++ Products Service/Program.cs
@@ -1,6 +1,28 @@
+using LaMaCo.Comments.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Products_Service.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ProductDbContext>(options =>
+{
+      if (builder.Environment.IsDevelopment())
+      {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            var dbPath = System.IO.Path.Join(path, "comments.db");
+            options.UseSqlite($"Data Source={dbPath}");
+            options.EnableDetailedErrors();
+            options.EnableSensitiveDataLogging();
+      }
+      else
+      {
+            var cs = builder.Configuration.GetConnectionString("CommentsContext");
+            options.UseSqlServer(cs);
+      }
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,12 +31,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+      var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+      db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
       app.UseSwagger();
       app.UseSwaggerUI();
 }
+
 
 app.UseAuthorization();
 
