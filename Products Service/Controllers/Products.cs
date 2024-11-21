495fda2d289eb1d716b1e3b8cd9d6ba7dd615aa0
KunoDLK <kunodlk@gmail.com>
2024-11-21 11:17:13 +0000

    added unit tests and added to workflow (#2) (#3)
    * Added database
    
    * added unit test to solution

---
+++ Products Service/Controllers/Products.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products_Service.Data;
+using LaMaCo.Comments.Api.Data;
+
+namespace Products_Service.Controllers
+{
+      [Route("api/[controller]")]
+      [ApiController]
+      public class Products : ControllerBase
+      {
+            private readonly ProductDbContext _db;
+
+            public Products(ProductDbContext dbContexts)
+            {
+                  _db = dbContexts;
+            }
+
+            // GET: api/Products
+            [HttpGet]
+            public ActionResult<IEnumerable<Product>> GetProducts()
+            {
+                  return _db.Products.ToList();
+            }
+
+            // GET: api/Products/5
+            [HttpGet("{id}")]
+            public ActionResult<Product> GetProductById(int id)
+            {
+                  var product = _db.Products.Find(id);
+                  if (product == null)
+                  {
+                        return NotFound();
+                  }
+                  return product;
+            }
+
+            // POST: api/Products
+            [HttpPost]
+            public ActionResult<Product> CreateProduct(Product product)
+            {
+                  _db.Products.Add(product);
+                  _db.SaveChanges();
+
+                  return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+            }
+
+            // PUT: api/Products/5
+            [HttpPut("{id}")]
+            public IActionResult UpdateProduct(int id, Product product)
+            {
+                  if (id != product.Id)
+                  {
+                        return BadRequest();
+                  }
+
+                  _db.Entry(product).State = EntityState.Modified;
+                  try
+                  {
+                        _db.SaveChanges();
+                  }
+                  catch (DbUpdateConcurrencyException)
+                  {
+                        if (!_db.Products.Any(e => e.Id == id))
+                        {
+                              return NotFound();
+                        }
+                        else
+                        {
+                              throw;
+                        }
+                  }
+
+                  return NoContent();
+            }
+
+            // DELETE: api/Products/5
+            [HttpDelete("{id}")]
+            public IActionResult DeleteProduct(int id)
+            {
+                  var product = _db.Products.Find(id);
+                  if (product == null)
+                  {
+                        return NotFound();
+                  }
+
+                  _db.Products.Remove(product);
+                  _db.SaveChanges();
+
+                  return NoContent();
+            }
+      }
+}
---
+++ Products Service/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿using LaMaCo.Comments.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace Products_Service.Data
+{
+      public class Product
+      {
+            public int Id { get; set; }
+            public string Name { get; set; } = String.Empty;
+
+            public decimal Price { get; set; }  
+      }
+}
